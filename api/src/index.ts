6156b22349fdd85f16761d8c96d3e38ce772be44
TezzaMan <terry-ochodnicky@hotmail.co.uk>
2024-10-17 20:16:15 +0100

    products router boilerplate setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, {Router} from 'express';
+//import router from './routes/products';
+import productsRouter from './routes/products';
 
 //const express = require('express') 
 const app = express()
@@ -7,6 +9,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//products endpoints, now in products/index.ts
+
+app.use("/products", productsRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
