88db2feb6f253c12476de17e769c59eb26b2a42d
TezzaMan <terry-ochodnicky@hotmail.co.uk>
2024-10-19 19:51:17 +0100

    Products router and controllers

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,19 +1,18 @@
 import { Router } from "express"
+import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from "./productsController"
 
 // products endpoints
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('The list of products')
-})
-
-router.get('/:id', (req, res) => {
-  console.log(req.params)
-  res.send('A product with the id')
-})
-
-router.post('/', (req, res) => {
-  res.send('New product created')
-})
+//router.get('/', (req, res) => {
+//  res.send('The list of products')
+//}) In the above, the 2nd parameter is a function, so we placed it in the productsController.ts
+//file, and then imported it as listProducts above, we did the same for the other requests below
+//Note that we remove the => when in productsController.ts
+router.get('/', listProducts)
+router.get('/:id', getProductById)
+router.post('/', createProduct)
+router.put('/:id', updateProduct)
+router.delete('/:id', deleteProduct)
 
 export default router
---
+++ api/src/routes/products/productsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+
+export function listProducts(req: Request, res: Response) {
+    res.send('The list of products')
+}
+
+export function getProductById(req: Request, res: Response) {
+    res.send('Get the product by its id')
+}
+
+export function createProduct(req: Request, res: Response) {
+    res.send('Create a product')
+}
+
+export function updateProduct(req: Request, res: Response) {
+    res.send('Update a product')
+}
+
+export function deleteProduct(req: Request, res: Response) {
+    res.send('Delete a product')
+}
